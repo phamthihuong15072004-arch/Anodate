475ad25b1009f4d69256d92cd376d9d6c90b52f5
phamthihuong15072004-arch <phamthihuong15072004@gmail.com>
2026-02-28 13:02:14 +0700

    Update server.js

--- server.js
+++ server.js
@@ -1,49 +1,33 @@
 const express = require("express");
 const http = require("http");
-const socketIO = require("socket.io");
+const socketIo = require("socket.io");
+const path = require("path");
 
 const app = express();
 const server = http.createServer(app);
-const io = socketIO(server);
+const io = socketIo(server);
 
-app.use(express.static(__dirname));
+const PORT = process.env.PORT || 3000;
 
-let waitingUser = null;
+// Cho phép hiển thị index.html
+app.use(express.static(path.join(__dirname)));
 
-io.on("connection", (socket) => {
-    console.log("User connected:", socket.id);
-
-    socket.on("findPartner", () => {
-        if (waitingUser) {
-            socket.partner = waitingUser;
-            waitingUser.partner = socket;
-
-            waitingUser = null;
-        } else {
-            waitingUser = socket;
-        }
-    });
-
-    socket.on("chatMessage", (msg) => {
-        if (socket.partner) {
-            socket.partner.emit("chatMessage", "Người lạ: " + msg);
-        }
-    });
-
-    socket.on("disconnect", () => {
-        if (socket.partner) {
-            socket.partner.emit("chatMessage", "Người lạ đã rời đi.");
-            socket.partner.partner = null;
-        }
-
-        if (waitingUser === socket) {
-            waitingUser = null;
-        }
-
-        console.log("User disconnected:", socket.id);
-    });
+app.get("/", (req, res) => {
+  res.sendFile(path.join(__dirname, "index.html"));
 });
 
-server.listen(3000, () => {
-    console.log("Server running on http://localhost:3000");
+io.on("connection", (socket) => {
+  console.log("User connected");
+
+  socket.on("chat message", (msg) => {
+    io.emit("chat message", msg);
+  });
+
+  socket.on("disconnect", () => {
+    console.log("User disconnected");
+  });
+});
+
+server.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
 });
