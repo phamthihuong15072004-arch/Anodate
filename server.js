1ab8a0a630421f8e8c4dc38d39aa2f2123a7795e
phamthihuong15072004-arch <phamthihuong15072004@gmail.com>
2026-02-28 14:15:57 +0700

    Update server.js

--- server.js
+++ server.js
@@ -8,86 +8,79 @@ const io = new Server(server);
 
 app.use(express.static("public"));
 
-let users = {};   // Lưu thông tin user
-let waitingUsers = [];  // Hàng chờ
+let users = {};
+let waitingUsers = [];
 
 io.on("connection", (socket) => {
-    console.log("User connected:", socket.id);
 
-    // Khi user tìm người
-    socket.on("findPartner", ({ myGender, findGender }) => {
+socket.on("findPartner", ({ myGender, findGender }) => {
 
-        // Lưu thông tin user
-        users[socket.id] = {
-            id: socket.id,
-            myGender,
-            findGender,
-            partner: null
-        };
+users[socket.id] = {
+id: socket.id,
+myGender,
+findGender,
+partner: null
+};
 
-        // Tìm người phù hợp trong hàng chờ
-        let foundIndex = waitingUsers.findIndex(otherId => {
-            const other = users[otherId];
-            if (!other) return false;
+let foundIndex = waitingUsers.findIndex(otherId => {
+const other = users[otherId];
+if (!other) return false;
 
-            return (
-                (findGender === "Any" || other.myGender === findGender) &&
-                (other.findGender === "Any" || myGender === other.findGender)
-            );
-        });
+return (
+(findGender === "Any" || other.myGender === findGender) &&
+(other.findGender === "Any" || myGender === other.findGender)
+);
+});
 
-        if (foundIndex !== -1) {
-            const partnerId = waitingUsers[foundIndex];
+if (foundIndex !== -1) {
+const partnerId = waitingUsers[foundIndex];
+waitingUsers.splice(foundIndex,1);
 
-            // Xoá khỏi hàng chờ
-            waitingUsers.splice(foundIndex, 1);
+users[socket.id].partner = partnerId;
+users[partnerId].partner = socket.id;
 
-            // Lưu partner cho cả hai
-            users[socket.id].partner = partnerId;
-            users[partnerId].partner = socket.id;
+io.to(socket.id).emit("matched", users[partnerId].myGender);
+io.to(partnerId).emit("matched", users[socket.id].myGender);
 
-            // Báo match
-            io.to(socket.id).emit("matched", users[partnerId].myGender);
-            io.to(partnerId).emit("matched", users[socket.id].myGender);
+} else {
+waitingUsers.push(socket.id);
+socket.emit("waiting");
+}
+});
 
-        } else {
-            // Nếu chưa tìm được thì thêm vào hàng chờ
-            waitingUsers.push(socket.id);
-            socket.emit("waiting");
-        }
-    });
+socket.on("chatMessage", (msg) => {
+const partnerId = users[socket.id]?.partner;
+if (partnerId) {
+io.to(partnerId).emit("chatMessage", msg);
+}
+});
 
-    // Nhận tin nhắn
-    socket.on("chatMessage", (msg) => {
-        const partnerId = users[socket.id]?.partner;
-        if (partnerId) {
-            io.to(partnerId).emit("chatMessage", msg);
-        }
-    });
+socket.on("leaveChat", () => {
+const partnerId = users[socket.id]?.partner;
 
-    // 🔥 Khi người dùng thoát
-    socket.on("disconnect", () => {
-        console.log("User disconnected:", socket.id);
+if (partnerId && users[partnerId]) {
+io.to(partnerId).emit("partnerLeft");
+users[partnerId].partner = null;
+}
 
-        const partnerId = users[socket.id]?.partner;
+users[socket.id].partner = null;
+});
 
-        // Nếu có partner thì thông báo cho người kia
-        if (partnerId && users[partnerId]) {
-            io.to(partnerId).emit("partnerLeft");
+socket.on("disconnect", () => {
+const partnerId = users[socket.id]?.partner;
 
-            // Xoá partner của người kia
-            users[partnerId].partner = null;
-        }
+if (partnerId && users[partnerId]) {
+io.to(partnerId).emit("partnerLeft");
+users[partnerId].partner = null;
+}
 
-        // Xoá khỏi hàng chờ nếu đang chờ
-        waitingUsers = waitingUsers.filter(id => id !== socket.id);
+waitingUsers = waitingUsers.filter(id => id !== socket.id);
+delete users[socket.id];
+});
 
-        // Xoá user
-        delete users[socket.id];
-    });
 });
 
 const PORT = process.env.PORT || 3000;
 server.listen(PORT, () => {
-    console.log("Server running on port", PORT);
+console.log("Server running on port", PORT);
 });
