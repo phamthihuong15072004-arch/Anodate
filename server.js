20fc8d6c40fdc68e747c6dca92b1ebecb5048a90
phamthihuong15072004-arch <phamthihuong15072004@gmail.com>
2026-02-28 13:18:06 +0700

    Update server.js

--- server.js
+++ server.js
@@ -9,25 +9,59 @@ const io = socketIo(server);
 
 const PORT = process.env.PORT || 3000;
 
-// Cho phép hiển thị index.html
 app.use(express.static(path.join(__dirname)));
 
 app.get("/", (req, res) => {
   res.sendFile(path.join(__dirname, "index.html"));
 });
 
-io.on("connection", (socket) => {
-  console.log("User connected");
+let waitingUsers = [];
 
-  socket.on("chat message", (msg) => {
-    io.emit("chat message", msg);
+io.on("connection", (socket) => {
+
+  socket.on("findPartner", (data) => {
+
+    socket.myGender = data.myGender;
+    socket.findGender = data.findGender;
+
+    const matchIndex = waitingUsers.findIndex(user => {
+      return (
+        (socket.findGender === "Any" || user.myGender === socket.findGender) &&
+        (user.findGender === "Any" || socket.myGender === user.findGender)
+      );
+    });
+
+    if (matchIndex !== -1) {
+      const partner = waitingUsers.splice(matchIndex, 1)[0];
+
+      socket.partner = partner;
+      partner.partner = socket;
+
+      socket.emit("matched", partner.myGender);
+      partner.emit("matched", socket.myGender);
+
+    } else {
+      waitingUsers.push(socket);
+      socket.emit("waiting");
+    }
+  });
+
+  socket.on("chatMessage", (msg) => {
+    if (socket.partner) {
+      socket.partner.emit("chatMessage", msg);
+    }
   });
 
   socket.on("disconnect", () => {
-    console.log("User disconnected");
+    if (socket.partner) {
+      socket.partner.emit("partnerLeft");
+      socket.partner.partner = null;
+    }
+    waitingUsers = waitingUsers.filter(user => user.id !== socket.id);
   });
+
 });
 
 server.listen(PORT, () => {
-  console.log(`Server running on port ${PORT}`);
+  console.log("Server running on port " + PORT);
 });
